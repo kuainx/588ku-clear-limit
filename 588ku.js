44daedce43ef755e9c9e736227a6a111dfe8b50b
KUAI <2542862169@qq.com>
2021-08-14 22:27:10 +0800

    Create 588ku.js

---
+++ 588ku.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         588ku clear
+// @namespace    http://ekuai.tech/
+// @version      0.1
+// @description  clear login limit page in 588ku.com
+// @author       kuai
+// @match        https://588ku.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  function scrollStyle() {
+    if (document.getElementById("scrollBarCustom")) {
+      return;
+    }
+    let s = document.createElement("style");
+    s.id = "scrollBarCustom";
+    s.innerHTML = `.down-limit {
+			display:none !important;
+		}`;
+    document.body.appendChild(s)
+    document.removeEventListener("scroll", scrollStyle);
+  }
+  document.addEventListener("scroll", scrollStyle);
+  scrollStyle();
+
+})();
